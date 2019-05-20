e1152fc55f5d8bf8fbf8a29e4fb921880c0cbb76
Donderstal <daanonderstal@hotmail.com>
2019-05-20 10:40:38 +0430

    add option items controller

--- app/Http/Controllers/Admin/Products/ProductsOptionsController.php
+++ app/Http/Controllers/Admin/Products/ProductsOptionsController.php
@@ -4,56 +4,65 @@ namespace App\Http\Controllers\Admin\Products;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CreateOptionGroupRequest;
+use App\Http\Requests\CreateOptionRequest;
 use App\Http\Requests\UpdateOptionGroupRequest;
+use App\Http\Requests\UpdateOptionRequest;
 use App\Models\Products;
+use App\Models\ProductsOption;
 use App\Models\ProductsOptionGroup;
 use Illuminate\Http\Request;
 
 class ProductsOptionsController extends Controller
 {
-    public function index(Products $product)
+    public function index(Products $product, ProductsOptionGroup $group)
     {
-        return view('admin.products.option_group')->with([
-            'groups' => $product->options()->orderBy('title', 'ASC')->get(),
-            'product' => $product
-        ]);
-    }
-
-    public function store(Products $product, CreateOptionGroupRequest $request)
-    {
-        $product->options()->create([
-            'title' => $request->title
-        ]);
-
-        flash_message('Option group added successfully.');
-
-        return redirect()->route('admin.products.option.index', $product);
-    }
-
-    public function edit(Products $product, ProductsOptionGroup $group)
-    {
-        return view('admin.products.option_group')->with([
-            'groups' => $product->options()->orderBy('title', 'ASC')->get(),
+        return view('admin.products.option')->with([
+            'options' => $group->items()->orderBy('title', 'ASC')->get(),
             'product' => $product,
             'group' => $group
         ]);
     }
 
-    public function update(Products $product, ProductsOptionGroup $group, UpdateOptionGroupRequest $request)
+    public function store(Products $product, ProductsOptionGroup $group, CreateOptionRequest $request)
     {
-        $group->update($request->only('title'));
+        $group->items()->create([
+            'title' => $request->title
+        ]);
 
-        flash_message('Option group updated successfully.');
+        flash_message('Option item added successfully.');
 
-        return redirect()->route('admin.products.option.index', $product);
+        return redirect()->route('admin.products.option.items.index', [$product, $group]);
     }
 
-    public function delete(Products $product, ProductsOptionGroup $group)
+    public function edit(Products $product, ProductsOptionGroup $group, ProductsOption $option)
     {
-        $group->delete();
+        return view('admin.products.option')->with([
+            'options' => $group->items()->orderBy('title', 'ASC')->get(),
+            'product' => $product,
+            'group' => $group,
+            'option' => $option
+        ]);
+    }
 
-        flash_message('Option group deleted successfully.', 'warning');
+    public function update(
+        Products $product,
+        ProductsOptionGroup $group,
+        ProductsOption $option,
+        UpdateOptionRequest $request
+    ) {
+        $option->update($request->only('title'));
 
-        return redirect()->route('admin.products.option.index', $product);
+        flash_message('Option item updated successfully.');
+
+        return redirect()->route('admin.products.option.items.index', [$product, $group]);
+    }
+
+    public function delete(Products $product, ProductsOptionGroup $group, ProductsOption $option)
+    {
+        $option->delete();
+
+        flash_message('Option item deleted successfully.', 'warning');
+
+        return redirect()->route('admin.products.option.items.index', [$product, $group]);
     }
 }
