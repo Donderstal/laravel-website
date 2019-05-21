b5eead35e5b5e428e66bb196a21973365f5c45a8
Donderstal <daanonderstal@hotmail.com>
2019-05-21 16:19:36 +0430

    make note nullable

--- database/migrations/2019_05_11_145858_create_products_table.php
+++ database/migrations/2019_05_11_145858_create_products_table.php
@@ -16,7 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('title');
-            $table->text('note');
+            $table->text('note')->nullable();
             $table->bigInteger('cover_id')->unsigned()->nullable();
             $table->double('price');
             $table->double('mileage');
