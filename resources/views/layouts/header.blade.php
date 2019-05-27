e503f896f4f2865d9182133ab6ff9ccec88f0cf8
Donderstal <daanonderstal@hotmail.com>
2019-05-27 13:52:42 +0200

    small changes...

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,13 @@
+<section>
+    <div class="top-bar grid-x">
+        <div class="xsmall-3">
+            <p><u>M</u>enu</p>
+        </div>
+        <div class="xsmall-6">
+            <p><h3>G A M</h3></p>
+        </div>
+        <div class="xsmall-3">
+            <p><i class="fa fa-search " ></i></p>
+        </div>
+    </div>
+</section>
