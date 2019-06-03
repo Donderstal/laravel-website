e5566a109c0b19171b03fa88eaae88aeea93d7a2
Donderstal <daanonderstal@hotmail.com>
2019-06-03 16:03:17 +0200

    added footer fonts, basic footer structure

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,20 +1,65 @@
 <section class="footer">
-    <!-- 
-       Contact information 
-    -->
-    <div class="footer__contact-info">
-    </div>
 
     <!-- 
-       Photo and logo 
+       Contact and photo
     -->
-    <div class="footer__foto-and-logo">
+    <div class="grid-x footer__contact-and-photo">
+         <div class="cell small-12 large-5 grid-x footer__contact">
+
+            <!--- Footer header --->
+            <div class="cell small-12"> 
+               <h2>CONTACT</h2>
+            </div>
+
+            <!--- Contact details --->
+            <div class="cell small-12 medium-4 large-6"> 
+               <div class="footer__contact-details">
+                  <p><i class="fas fa-map-marker-alt"></i><span> Energiestraat 25B <br/>
+                  <u>1</u>411 AR, Naarden </span></p>
+               </div>
+               <div class="footer__contact-details">
+                  <p><i class="fas fa-phone"></i><span> <u>0</u>35 - 694 4646</span></p>
+               </div>
+               <div class="footer__contact-details">
+                  <p><i class="far fa-envelope"></i><span> <u>i</u>nfo@gambv.nl</span></p>
+               </div>
+            </div>
+
+            <!--- Opening hours --->
+            <div class="cell small-12 medium-4 large-6"> 
+               <div class="footer__opening-hours"> 
+                  <p class="footer__opening-hours__day"> Maandag t/m vrijdag </p>
+                  <p class="footer__opening-hours__type"> Showroom & werkplaats </p>
+                  <p class="footer__opening-hours__time"> 08:00 - 18:00 </p>
+               </div>
+               <div class="footer__opening-hours"> 
+                  <p class="footer__opening-hours__day"> Zaterdag </p>
+                  <p class="footer__opening-hours__type"> Showroom </p>
+                  <p class="footer__opening-hours__time"> 10:00 - 17:00 </p>
+                  <p class="footer__opening-hours__type"> Werkplaats </p>
+                  <p class="footer__opening-hours__time"> Gesloten </p>
+               </div>
+               <div class="footer__opening-hours"> 
+                  <p class="footer__opening-hours__day"> Zondag </p>
+                  <p class="footer__opening-hours__type"> Showroom & werkplaats </p>
+                  <p class="footer__opening-hours__time"> Gesloten </p>
+               </div>
+            </div>
+
+         </div>
+
+         <!-- photo with logo -->
+         <div class="cell small-12 large-7 footer__photo">
+               dikke foto jonge
+         </div>
+
     </div>
 
     <!-- 
        Newsletter and social
     -->
     <div class="footer__newsletter-and-social">
+
     </div>
 
 </section>
