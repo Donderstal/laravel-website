2ca23e067e0e7c893dfb9435aee07955823ed681
Donderstal <daanonderstal@hotmail.com>
2019-06-04 12:18:33 +0200

    adding gutters

--- resources/views/aanbod.blade.php
+++ resources/views/aanbod.blade.php
@@ -4,7 +4,7 @@
 @section('content')
     <section class="ons-aanbod">
         <div class="grid-container">
-            <div class="grid-x">
+            <div class="grid-x grid-margin-x grid-margin-y">
                 @foreach($products as $indexKey => $product)
                     @include('products.card')
                 @endforeach
--- resources/views/products/card.blade.php
+++ resources/views/products/card.blade.php
@@ -1,4 +1,4 @@
-<div class="cell large-4">
+<div class="product-card cell large-4">
     <h3>{{ $product->title }}</h3>
     <p>Dikke waggie jonguh</p>
 </div>
