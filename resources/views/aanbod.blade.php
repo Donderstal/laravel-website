364653c9821217910ca6651390a5ad617fe29d84
Donderstal <daanonderstal@hotmail.com>
2019-06-07 16:01:05 +0200

    working on product card styling

--- resources/views/aanbod.blade.php
+++ resources/views/aanbod.blade.php
@@ -3,6 +3,36 @@
 
 @section('content')
     <section class="ons-aanbod">
+
+        <div class="grid-x homepage__search-bar">
+            <div class="cell small-12 medium-8 search-bar__left">
+                <span class="search-bar__title">Vind jouw merk</a>
+                <img class="search-bar__icon" src="{{ asset('img/ui-icons/list-arrows.svg')}}">
+            </div>
+            <div class="cell small-12 medium-4 search-bar__right">
+                <button class="search-bar__button">ZOEKEN</button>
+            </div>
+        </div>
+
+        <div class="grid-x ons-aanbod">
+
+            <div class="cell small-12 medium-6">
+                <h2 class="ons-aanbod__header">ONS AANBOD</h2>
+                <h2 class="ons-aanbod__header-number"> 00 </h2>
+            </div>
+
+            <div class="cell small-12 medium-6">
+                <span class="ons-aanbod__sort-text"> Sorteren op </span>
+                <select class="ons-aanbod__sort-select">    
+                    <option value="volvo">Volvo</option>
+                    <option value="saab">Saab</option>
+                    <option value="mercedes">Mercedes</option>
+                    <option value="audi">Audi</option>
+                </select>
+            </div>
+
+        </div>
+
         <div class="grid-container">
             <div class="grid-x grid-margin-x grid-margin-y">
                 @foreach($products as $indexKey => $product)
--- resources/views/products/card.blade.php
+++ resources/views/products/card.blade.php
@@ -1,10 +1,29 @@
-<div class="cell small-12 large-4">
+<div class="cell small-12 medium-6 large-4 xlarge-3">
 
-    <div class="product-card ">
+    <div class="grid-y product-card ">
 
-    <h3>Test test</h3>
-
-    <p>Dikke waggie jonguh</p>
+        <div class="cell small-7 product-card__image-wrapper">
+            <img class="product-card__car-image" src="{{ asset('img/nice-car.jpeg') }}">
+        </div>
+        <div class="cell small-3 grid-x product-card__top-text-row">
+            <div class="cell small-12 grid-x">
+                <div>Audi</div> <div>59.000</div>
+                <div>A5 Cabriolet 2.0</div> 
+            </div>
+        </div>
+        <div class="cell small-2 grid-x product-card__bottom-text-row">
+            <div class="cell small-4">
+                <div>KM stand</div>
+                <div>honderdmiljoen</div>
+            </div>
+            <div class="cell small-4">
+                <div>Bouwjaar</div>
+                <div>2018</div>
+            </div>
+            <div class="cell small-4">
+                <div><u>BE</u>KIJKEN</div>
+            </div>
+        </div>
 
     </div>
 
