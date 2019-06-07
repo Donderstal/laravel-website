2b3c760735b48730cb136c21d2583aa9f66ff4bd
Donderstal <daanonderstal@hotmail.com>
2019-06-07 16:43:02 +0200

    updating card fonts, styling

--- resources/views/products/card.blade.php
+++ resources/views/products/card.blade.php
@@ -7,21 +7,25 @@
         </div>
         <div class="cell small-3 grid-x product-card__top-text-row">
             <div class="cell small-12 grid-x">
-                <div>Audi</div> <div>59.000</div>
-                <div>A5 Cabriolet 2.0</div> 
+                <div class="cell small-6 product-card__car-name">Audi</div> 
+                <div class="cell small-6 product-card__car-price">59.000</div>
+                <div class="cell small-12 product-card__car-model"v>A5 Cabriolet 2.0</div> 
+            </div>
+            <div class="product-card__decorative-line">
             </div>
         </div>
+        
         <div class="cell small-2 grid-x product-card__bottom-text-row">
             <div class="cell small-4">
-                <div>KM stand</div>
-                <div>honderdmiljoen</div>
+                <div class="product-card__kilometers-header">KM stand</div>
+                <div class="product-card__kilometers">1.000.000</div>
             </div>
             <div class="cell small-4">
-                <div>Bouwjaar</div>
-                <div>2018</div>
+                <div class="product-card__year-header">Bouwjaar</div>
+                <div class="product-card__year">2018</div>
             </div>
             <div class="cell small-4">
-                <div><u>BE</u>KIJKEN</div>
+                <div class="product-card__check-me-out"><u>BE</u>KIJKEN</div>
             </div>
         </div>
 
