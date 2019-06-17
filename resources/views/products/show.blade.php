0efb01009d9101d9ac0d64b609217a7702fcf79d
Donderstal <daanonderstal@hotmail.com>
2019-06-17 15:39:06 +0200

    finished applying fonts, basic styling for mobile header

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -17,20 +17,91 @@
 
         <div class="cell small-12 large-6 product-page__main-info">
             <div class="product-page__main-info__inner">
-                {{-- Product Title --}}
-                <h1>{{ $product->title }}</h1>
 
-                {{-- Product Brand --}}
-                <p>{{ $product->brand->title }}</p>
+                <div class="product-page__main-info__subsection">
+                    <h1 class="product-page__main-header-font">{{ $product->brand->title }}</h1>
+
+                    <p class="product-page__main-subtitle-font">{{ $product->title }}</p>
+                </div>
+
+                <div class="product-page__main-info__subsection">
+                    <p class="product-page__price-font">&euro; {{ number_format($product->price) }}</p>
+
+                    <p class="product-page__VAT-font">BTW niet verrekenbaar</p>
+                </div>
+
+                <div class="grid-x product-page__main-info__subsection">
+
+                    <div class="cell small-12">
+                        <h3 class="product-page__main-subtitle-font">Interesse?</h3>                        
+                    </div>
+
+                    <div class="cell small-6">
+                        <div class="product-page__contact-small-font">Bellen </div>
+                        <div>
+                            <a class="product-page__contact-large-font" href="tel:+31356944646"><u>0</u>35 694 4646</a> 
+                        </div>
+                    </div>
+
+                    <div class="cell small-4 end">
+                        <div class="product-page__contact-small-font">Delen </div>
+                        <div class="product-page__social-icons-wrapper"> 
+                            <img class="svg-injection" src="{{ asset('img/ui-icons/facebook.svg') }}">
+                            <img class="svg-injection" src="{{ asset('img/ui-icons/link.svg') }}"> 
+                            <img class="svg-injection" src="{{ asset('img/ui-icons/mail.svg') }}">
+                        </div>
+                    </div>
+
+                    <div class="cell small-12 product-page__decorative-line">
+                    </div>
+
+                    <div class="cell small-12 medium-6">
+                        <input class="product-page__input" placeholder="Naam">
+                    </div>
+
+                    <div class="cell small-12 medium-6">
+                        <input class="product-page__input" placeholder="Telefoon nr.">
+                    </div>
+
+                    <div class="cell small-12 product-page__contact-large-font">
+                        <u>Bel</u> mij terug
+                    </div>
+                    
+                </div>
+
+                <div class="grid-x product-page__main-info__subsection">
+                    <div class="cell small-6">
+                        <p class="product-page__detail-title-darkbg-font">Km stand</p>
+                        <p class="product-page__detail-darkbg-font">{{ $product->mileage }} km</p>
+                    </div>  
+
+                    <div class="cell small-6">
+                        <p class="product-page__detail-title-darkbg-font">Brandstof</p>
+                        <p class="product-page__detail-darkbg-font">{{ config('site.products.fuel_types')[$product->fuel] }}</p>
+                    </div>  
+
+                    <div class="cell small-6">
+                        <p class="product-page__detail-title-darkbg-font">Kleur</p>
+                        <p class="product-page__detail-darkbg-font">{{ $product->color->title }}</p>
+                    </div>  
+
+                    <div class="cell small-6">
+                        <p class="product-page__detail-title-darkbg-font">Transmissie</p>
+                        <p class="product-page__detail-darkbg-font">{{ config('site.products.transmission_types')[$product->transmission] }}</p>
+                    </div>  
+                </div>
+
+                <div class="product-page__main-info__subsection">
+                    <p class="product-page__all-specs-font"><img class="product-page__all-specs-arrow svg-injection" src="{{ asset('img/ui-icons/arrow.svg') }}"> <u>BE</u>KIJK ALLE SPECIFICATIES</p>
+                </div>
 
                 {{-- Product Model --}}
-                <p>{{ $product->model->title }}</p>
+                <!-- <p>{{ $product->model->title }}</p>
 
                 {{-- Product Color --}}
-                <p>{{ $product->color->title }}</p>
-
+                <p>{{ $product->color->title }}</p>                
                 {{-- Product Price --}}
-                <p>{{ number_format($product->price) }}</p>
+                
 
                 {{-- Product Millage --}}
                 <p>{{ $product->mileage }}</p>
@@ -42,7 +113,7 @@
                 <p>{{ config('site.products.transmission_types')[$product->transmission] }}</p>
 
                 {{-- Product Transmission --}}
-                <p>{{ config('site.products.fuel_types')[$product->fuel] }}</p>
+                <p>{{ config('site.products.fuel_types')[$product->fuel] }}</p> -->
             </div>
         </div>
 
