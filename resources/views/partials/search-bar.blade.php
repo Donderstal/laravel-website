e0dcbee76e8633ac9231c136c3b1942625ad9d68
Donderstal <daanonderstal@hotmail.com>
2019-08-05 16:32:36 +0200

    Search Bar partial now includes brand names as options

--- resources/views/partials/search-bar.blade.php
+++ resources/views/partials/search-bar.blade.php
@@ -2,16 +2,9 @@
     <div class="cell small-12 medium-7 search-bar__left">
         <select class="search-bar__title">
             <option value="" disabled selected>Vind jouw merk</option>
-            <option class="search-bar__option" value="audi">Audi</option>
-            <option class="search-bar__option" value="bmw">BMW</option>
-            <option class="search-bar__option" value="bentley">Bentley</option>
-            <option class="search-bar__option" value="citroën">Citroën</option>
-            <option class="search-bar__option" value="ferrari">Ferrari</option>
-            <option class="search-bar__option" value="land-rover">Land Rover</option>
-            <option class="search-bar__option" value="mini">MINI</option>
-            <option class="search-bar__option" value="mercedes-benz">Mercedes-Benz</option>
-            <option class="search-bar__option" value="porsche">Porsche</option>
-            <option class="search-bar__option" value="volvo">Volvo</option>
+            @foreach($brands as $indexKey => $brand)
+                <option class="search-bar__option" value="{{ strtolower($brand) }}">{{ $brand }}</option>
+            @endforeach
         </select>
         <img class="search-bar__icon svg-injection" src="{{ asset('img/ui-icons/list-arrows.svg')}}">
     </div>
