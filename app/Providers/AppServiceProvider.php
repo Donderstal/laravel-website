b14cd5f7e2fbf54fc224dfcef6ab93a4fe8a06cd
Donderstal <daanonderstal@hotmail.com>
2019-10-18 13:08:39 +0200

    Some changes to optimize cloning and running the project locally

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -32,7 +33,7 @@ class AppServiceProvider extends ServiceProvider
         if (env('APP_ENV') == 'production' && env('ENABLE_HTTPS') == true) {
             URL::forceScheme('https');
         }
-
+        Schema::defaultStringLength(191);
         Blade::component('admin.components.fields.input', 'input');
         Blade::component('admin.components.button', 'button');
         Blade::component('admin.components.panel', 'panel');
